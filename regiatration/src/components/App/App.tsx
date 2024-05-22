a6f79f031483138ffdf69546ae52b1f212aff3d5
Viktoriya-ri <viktoriya23071@gmail.com>
2024-05-22 17:38:40 +0300

    x

--- regiatration/src/components/App/App.tsx
+++ regiatration/src/components/App/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {User} from './App.types';
 
-function App() {
+type Props = { 
+  user: User;
+}
+ export function App({user}: Props) {
   return (
     <div className="App">
+      <h1>{user.name}</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -23,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+
--- regiatration/src/index.tsx
+++ regiatration/src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {App} from './components/App/App';
+import {User} from './components/App/App.types';
 import reportWebVitals from './reportWebVitals';
 
+
+const user: User = {
+  id: 1,
+  name: 'Jon',
+  email: 'explorer@explorer.com',
+}
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <App user={user}/>
   </React.StrictMode>
 );
 
